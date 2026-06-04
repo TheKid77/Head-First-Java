49dce21c2b855b1e23a9f115a7f2a1a07ee969e2
TheKid77 <andymcc77@blueyonder.co.uk>
2026-06-04 16:14:51 +0100

    fruits

---
+++ 1-A-quick-dip/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        String [] fruits = {"Apple", "Banana", "Cherry"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
